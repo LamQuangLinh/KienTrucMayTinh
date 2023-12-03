5cab7d5471c246cd87279d12fe8cc11f9b00f2e5
Lâm Quang Linh <113756313+LamQuangLinh@users.noreply.github.com>
2023-12-03 17:41:29 +0700

    Add files via upload

---
+++ Kiến Trúc Máy Tính/2115207_DinhTrongHieu_Lab1/BaiTap/BT4.asm
@@ -0,0 +1,48 @@
+.MODEL SMALL
+  .STACK
+.DATA
+    NHAP DB 'Hay nhap mot ky tu: $'
+    XUAT DB 13,10,'KY TU DUNG TRUOC:$'
+     XUAT2 DB ', DA NHAP: $' 
+      XUAT3 DB ', KY TU DUNG SAU: $'
+    kt DB ?
+.CODE
+    MOV AX,@DATA
+    MOV DS,AX
+    
+    LEA DX,NHAP
+    MOV AH,09H
+    INT 21H
+    
+    MOV AH,01H
+    INT 21H
+    MOV kt,AL  
+    
+    LEA DX,XUAT
+     MOV AH,09H
+    INT 21H 
+    
+    MOV DL,kt
+    MOV AH,02H
+    DEC DL
+    INT 21H 
+    
+    LEA DX,XUAT2
+     MOV AH,09H
+    INT 21H  
+    
+     MOV DL, kt
+    MOV AH, 02H 
+    INT 21H 
+    
+     LEA DX,XUAT3
+     MOV AH,09H
+    INT 21H 
+    
+    MOV DL, kt
+    MOV AH, 02H 
+    INC DL
+    INT 21H 
+    
+    MOV AH,4CH
+    INT 21H
